193af9b82aeff1c3acd81c74c54dd831ea6903c7
peter001 <kaopeter@gmail.com>
2018-01-14 09:40:05 +0800

    merge 120

--- lib/leafletjs.util.dart
+++ lib/leafletjs.util.dart
@@ -171,3 +171,17 @@ class Dynamic<T> {
     return value;
   }
 }
+
+/// A workaround to converting an object from JS to a Dart Map.
+// Map jsToMap(jsObject) {
+//   return new Map.fromIterable(
+//     _getKeysOfObject(jsObject),
+//     value: (key) => getProperty(jsObject, key),
+//   );
+// }
+
+// Both of these interfaces exist to call `Object.keys` from Dart.
+//
+// But you don't use them directly. Just see `jsToMap`.
+// @JS('Object.keys')
+// external List<String> _getKeysOfObject(jsObject);
